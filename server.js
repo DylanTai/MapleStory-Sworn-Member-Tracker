cceab14507ee0220604fd45a6d4e7c7d9d6228d5
Dylan Tai <dylztai@gmail.com>
2025-09-16 09:10:29 -0400

    renamed

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(expressLayouts);
 app.set("layout", "layout");
 
 // Static + parsers + method override
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "styles")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
